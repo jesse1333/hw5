06a40f60c916278e29cbe680a0c51d710213817e
Jesse Tzo <tzo@usc.edu>
2024-04-16 21:30:25 +0000

    All done!

--- schedwork.cpp
+++ schedwork.cpp
@@ -22,8 +22,54 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
-
 // Add your implementation of schedule() and other helper functions here
+
+// Helper function to check if a worker has exceeded the maximum number of shifts
+
+
+bool scheduleHelper(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts,
+                    DailySchedule& schedule, size_t day, size_t shiftCount, vector<int> shifts, int workerIndex) {
+    // if all of the days are filled
+    if (day == avail.size()) {
+        return true;
+    }
+
+    if (shiftCount == dailyNeed) {
+        return scheduleHelper(avail , dailyNeed, maxShifts, schedule, day + 1, 0, shifts, 0);
+    }
+
+    if (workerIndex == avail[0].size()) {
+        return false;
+    }
+
+    if (avail[day][workerIndex] && shifts[workerIndex] < maxShifts) {
+        // assigns worker & updates shift
+        schedule[day].push_back(workerIndex);
+        shiftCount++;
+        shifts[workerIndex]++;
+
+        // moves onto next day
+        if(scheduleHelper(avail, dailyNeed, maxShifts, schedule, day, shiftCount, shifts, workerIndex + 1)) {
+            return true;
+        }
+
+        schedule[day].pop_back();
+        shiftCount--;
+        shifts[workerIndex]--;
+        if (scheduleHelper(avail, dailyNeed, maxShifts, schedule, day, shiftCount, shifts, workerIndex + 1)) {
+            return true;
+        }
+    }
+
+    else {
+        if (scheduleHelper(avail, dailyNeed, maxShifts, schedule, day, shiftCount, shifts, workerIndex + 1)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -37,9 +83,16 @@ bool schedule(
     }
     sched.clear();
     // Add your code below
+    int workerIndex = 0;
+    vector<int> shifts;
+    for (int i = 0; i < avail[0].size(); i++) {
+        shifts.push_back(0);
+    }
 
+    // resize the matrix to the same as available size
+    sched.resize(avail.size());   
 
-
-
+    return scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, 0, shifts, workerIndex);
 }
+
 
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void findCombinations(const std::string& in, const std::string& floating, int index, int count, std::string current, std::set<std::string>& combinations, const std::set<std::string>& dict, int numOfDashes);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -23,6 +23,78 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+    std::set<std::string> validWords;
+    int numOfDashes = 0;
+    for (int i = 0; i < in.size(); i++) {
+        if (in[i] == '-') {
+            numOfDashes++;
+        }
+    }
+
+    findCombinations(in, floating, 0, 0, in, validWords, dict, numOfDashes);
+    return validWords;
 }
 
 // Define any helper functions here
+void findCombinations(const std::string& in, const std::string& floating, int index, int count, std::string current, std::set<std::string>& combinations, const std::set<std::string>& dict, int numOfDashes) {
+    if (index == in.size()) {
+        // if not all characters are filled in, return
+        if (floating.size() != 0) {
+            return;
+        }
+
+        // if character is found in dictionary, we add it
+        else if (dict.find(current) != dict.end()) {
+            combinations.insert(current);
+            return;
+        }
+        return;
+    }
+
+    if (in[index] == '-') {
+
+        // if is a dash, iterate through every character in floating and call function recursively
+        if (numOfDashes == floating.size()) {
+            // if all the dashes are filled with floating characters
+            for (int i = 0; i < floating.size(); i++) {
+                current[index] = floating[i];
+                string floatingCopy = floating;
+                floatingCopy.erase(i, 1);
+
+                // put in the new string (update the floating)
+                findCombinations(in, floatingCopy, index + 1, count + 1, current, combinations, dict, numOfDashes - 1);
+            }
+        }
+        else {
+            // fills the rest of the - with non-floating characters
+            char letter = 'a';
+            for (int i = 0; i < 26; i++) {
+                bool copy = false;
+                string floatingCopy = floating;
+                // if the letter is the random one we want to update the floating
+
+                if (floating.find(letter) != string::npos) {
+                    copy = true;
+                    floatingCopy.erase(floating.find(letter), 1);
+                }
+
+                current[index] = letter;
+
+                if (copy) {
+                    findCombinations(in, floatingCopy, index + 1, count, current, combinations, dict, numOfDashes - 1);
+                }
+                else {
+                    findCombinations(in, floating, index + 1, count, current, combinations, dict, numOfDashes - 1);
+                }
+
+                ++letter;
+            }
+            current[index] = '-';
+        }
+
+    }
+    else {
+        // if not a dash, we just add the character to the current string being constructed & iterate the index
+        findCombinations(in, floating, index + 1, count, current, combinations, dict, numOfDashes);
+    }
+}
